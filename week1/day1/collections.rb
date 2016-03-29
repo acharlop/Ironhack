c4f087a9b2860182085c9dd6101711c9aa7de352
Avi Charlop <avicharlop@gmail.com>
2016-03-28 22:22:08 -0400

    Added end of day comments to exercise

--- week1/day1/collections.rb
+++ week1/day1/collections.rb
@@ -1,3 +1,4 @@
+# class definition
 class Home
 	attr_reader(:name, :city, :capacity, :price)
 
@@ -8,7 +9,7 @@ class Home
 		@price = price
 	end
 end
-
+# create array of "Home" objects
 homes = [
 	Home.new("Nizar", 	"San Juan", 	2, 42),
   Home.new("Fernando","Seville", 		5, 47),
@@ -21,7 +22,7 @@ homes = [
   Home.new("Ryan",		"New York",		6, 40),
   Home.new("Bianca",	"Miami",			2, 37)
 ]
-
+# function to print array nicely
 def print_homes(array)
 	array.each_with_index do |hm,idx|
 		puts "Home number #{idx+1}"
@@ -31,17 +32,21 @@ def print_homes(array)
 		puts
 	end
 end	
+# prompt user with some information
 system "clear"
 puts "Welcome to TxtBnb! We have a total of #{homes.length} options for you to stay!"
+# print doesn't end with newline
 print "Average capacity: "
+# for average reduce sum over all elements / num of elements
 puts homes.reduce(0.0){|sum,hm| sum + hm.capacity}  / homes.length
 print "Average cost: "
 puts homes.reduce(0.0){|sum,hm| sum + hm.price}  / homes.length
 puts "Press ENTER to continue"
 gets
-# 
+# loop through until only 1 value is left in array 
 while homes.length != 1
 	system "clear"
+	# use print function created earlier, prompt, and get input
 	print_homes(homes)
 	puts "Enter the listing number"
 	puts "Or to start sorting enter \'city\' or \'price\'"
@@ -49,16 +54,21 @@ while homes.length != 1
 	if sort_type.downcase == "city"
 		puts "Enter the city you would like to stay in:"
 		city = gets.chomp
+		# modify array self using bang and select only matching cities
 		homes.select!{|hm| hm.city.downcase == city.downcase}
 	elsif sort_type.downcase == "price"
 		puts "Enter the max price you would like to pay:"
 		price = gets.chomp
+		# modify array self using bang and select only less than pricing
 		homes.select!{|hm| hm.price <= price.to_i}
 	elsif sort_type.to_i.between?(0, homes.length)
+		# if array element is selected (between 0 and arr.length) element at
+		# return is still arrray for printing purposes
 		homes = [homes.at(sort_type.to_i-1)]
 	end
 end
 
+# clear and final prompt
 system "clear"
 puts "Congrats on finding your bed for the night!"
 puts "You will be staying at #{homes[0].name}\'s place in #{homes[0].city}"
