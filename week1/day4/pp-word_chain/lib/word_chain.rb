bd059a58a35e04355e9cfe62698bd3570d949317
Avi Charlop <avicharlop@gmail.com>
2016-03-31 16:40:53 -0400

    day 4 pp exercise - final recursive

--- week1/day4/pp-word_chain/lib/word_chain.rb
+++ week1/day4/pp-word_chain/lib/word_chain.rb
@@ -8,10 +8,12 @@ class WordChain
 
 	def find_chain(start_word,end_word)
 		if start_word.length != end_word.length
+			puts "Words need to be of equal length"
 			return
 		end
 		@start_word = start_word.dup
-		@end_word = end_word.dup
+		puts @start_word
+		@end_word = end_word
 		start = start_word.dup
 
 		@start_word.length.times do
@@ -20,19 +22,6 @@ class WordChain
 				return
 			end
 		end
-
-		for p in (0..@start_word.length) do
-			for letter in ("a".."z") do
-				start[p] = letter
-				@start_word.length.times do
-					start = try_word(start,0)
-					if start == @end_word
-						return
-					end
-				end				
-			end
-		end
-
 	end
 
 	def try_word(start,i)
@@ -41,7 +30,7 @@ class WordChain
 		else
 			test = start.dup
 			test[i] = @end_word[i]
-			if @dict.exists?(test) #&& @end_word[i] != start[i]
+			if @dict.exists?(test) && @end_word[i] != start[i]
 				start = test
 				puts start
 			end
