a88bc5f17c37717eebf066ffb43a4242ee1645bc
Avi Charlop <avicharlop@gmail.com>
2016-04-04 10:28:45 -0400

    Moved tests to RSpec run using \'rspec test.rb\'

--- week2/day1/test.rb
+++ week2/day1/test.rb
@@ -1,13 +1,51 @@
+require 'RSpec'
 require_relative "lib/string_calculator.rb"
-calc = StringCalculator.new
 
-puts calc.add("7,6,5,2") == 20
-puts calc.add("70,60,50,200,1") == 381
 
-puts calc.add("7,6") == 13
-puts calc.add("9,1") == 10
+RSpec.describe "String calculator" do
+	before :each do
+	@calc = StringCalculator.new
+end
+	specify "Returns 0 for the empty string" do
+		input = ""
+		output = 0
+		expect(@calc.add(input)).to eq(output)
+	end
 
-puts calc.add("5") == 5
-puts calc.add("3") == 3
+	it "Returns 3 for only that number" do
+		input = "3"
+		output = 3
+		expect(@calc.add(input)).to eq(output)
+	end
 
-puts calc.add("") == 0
+	it "Returns 5 for only that number" do
+		input = "5"
+		output = 5
+		expect(@calc.add(input)).to eq(output)
+	end
+
+	it "Returns 13 for two numbers" do
+		input = "7,6"
+		output = 13
+		expect(@calc.add(input)).to eq(output)
+	end
+
+	it "Returns 10 for two numbers" do
+		input = "9,1"
+		output = 10
+		expect(@calc.add(input)).to eq(output)
+	end
+
+	it "Returns 20 for 4 numbers" do
+		input = "7,6,5,2"
+		output = 20
+		expect(@calc.add(input)).to eq(output)
+	end
+
+	it "Returns 381 for 5 numbers" do
+		input = "70,60,50,200,1"
+		output = 381
+		expect(@calc.add(input)).to eq(output)
+	end
+end
+
