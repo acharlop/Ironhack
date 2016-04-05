1548807bf48449601fb9e02557156be759b122cc
Avi Charlop <avicharlop@gmail.com>
2016-04-05 17:10:59 -0400

    app.rb commits for multiple projects

--- week2/day2/pp-asciiart/app.rb
+++ week2/day2/pp-asciiart/app.rb
@@ -6,7 +6,7 @@ get "/ascii/:word/?:font?/?:secret?/?" do
 	if  params[:captures] == ["lion","special","secret"]
 		erb(:lion)
 	else
-		font_list = ["alligator","3-d","3x5","acrobatic","avatar","banner /3/4","cosmic","defleppard","doh","epic","fraktur"]
+		font_list = ["alligator","3-d","3x5","acrobatic","avatar","cosmic","doh","epic","fraktur"]
 		font = font_list.include?(params[:font]) ? params[:font] : 'slant'
 		my_ascii = Artii::Base.new :font => font
 		@word = my_ascii.asciify(params[:word])
