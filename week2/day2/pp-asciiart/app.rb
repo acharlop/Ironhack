456f9ef0a78454d7298b9db4e9140c12a7d86336
Avi Charlop <avicharlop@gmail.com>
2016-04-05 15:33:48 -0400

    Final version 3 of the ascii art web app using sinatra

--- week2/day2/pp-asciiart/app.rb
+++ week2/day2/pp-asciiart/app.rb
@@ -2,8 +2,14 @@ require "sinatra"
 require "artii"
 require "sinatra/reloader"
 
-get "/ascii/:word" do
-	my_ascii = Artii::Base.new :font => 'slant'
-	@word = my_ascii.asciify(params[:word])
-	erb(:ascii)
+get "/ascii/:word/?:font?/?:secret?/?" do
+	if  params[:captures] == ["lion","special","secret"]
+		erb(:lion)
+	else
+		font_list = ["alligator","3-d","3x5","acrobatic","avatar","banner /3/4","cosmic","defleppard","doh","epic","fraktur"]
+		font = font_list.include?(params[:font]) ? params[:font] : 'slant'
+		my_ascii = Artii::Base.new :font => font
+		@word = my_ascii.asciify(params[:word])
+		erb(:ascii)
+	end
 end
