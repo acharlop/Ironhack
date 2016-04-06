293172809c430f63b0ca80b2bbd1444b11a097c4
Avi Charlop <avicharlop@gmail.com>
2016-04-06 17:11:06 -0400

    Completed user login using a hash from user class

--- week2/day3/login/app.rb
+++ week2/day3/login/app.rb
@@ -2,29 +2,37 @@ require "sinatra"
 require "sinatra/reloader"
 require_relative "lib/user.rb"
 users = User.new
-enable(:session)
+enable(:sessions)
 
 get "/?" do
 	if session[:logged_in_user]
 		redirect to("/user/#{session[:logged_in_user]}")
+	else
+		erb(:login)
 	end
-	erb(:login)
 end
 
 post "/login" do
 	if users.validate_user(params[:username], params[:password])
 		session[:logged_in_user] = params[:username]
+		@user = params[:username]
+		redirect to("/user/#{session[:logged_in_user]}")
 	else
 		session[:logged_in_user] = nil
-		redirect to(:login)
+		erb(:login)
 	end
-	erb("/user/#{session[:logged_in_user]}")
 end
 
 get "/user/:username" do
 	if !session[:logged_in_user]
-		redirect to(:login)
-	end
+		erb(:login)
+	else
 		@user = session[:logged_in_user]
 		erb(:user)
+	end
+end
+
+post "/logout" do
+	session[:logged_in_user] = nil
+	erb(:login)
 end
