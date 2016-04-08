65a408252f36b2be008eff7dee7aef53d7a51740
Avi Charlop <avicharlop@gmail.com>
2016-04-08 15:56:56 -0400

    add each movie to list refractored out of build list method

--- week2/movie_lovers/lib/movie_search.rb
+++ week2/movie_lovers/lib/movie_search.rb
@@ -1,6 +1,8 @@
 require "imdb"
+require_relative "helpers.rb"
 # require_relative "imdb_movie.rb"
 class MovieSearch
+	include MovieSearchHelpers
 	attr_reader :list, :type
 	def initialize(search,type)
 		@search = Imdb::Search.new(search).movies
@@ -8,17 +10,28 @@ class MovieSearch
 		@type = type
 		build_list
 	end
+	# def build_list
+	# 	@search[0..8].each do | movie|
+	# 		poster = movie.poster
+	# 		if poster
+	# 			game_keys = self.send(@type, movie)
+	# 			@list.push({
+	# 				:poster=>poster,
+	# 				game_keys[0] => game_keys[1]
+	# 			})
+	# 		end
+	# 	end
+	# end
 	def build_list
 		@search[0..8].each do | movie|
-			poster = movie.poster
-			if poster
-				game_keys = self.send(@type, movie)
-				@list.push({
-					:poster=>poster,
-					game_keys[0] => game_keys[1]
-				})
-			end
+			add_to_list(movie)
 		end
+	end
+	def add_to_list(movie)
+		poster = movie.poster
+		return unless poster
+		key = self.send(@type, movie)
+		@list.push({:poster=>poster, key[0] => key[1]})
 	end
 	def year(movie)
 		[:year, movie.year]
