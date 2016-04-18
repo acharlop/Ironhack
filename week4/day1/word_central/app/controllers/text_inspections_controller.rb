b8063a096b8a56e0279cfc1b8331fe6db6873837
Avi Charlop <avicharlop@gmail.com>
2016-04-18 16:28:07 -0400

    Longest 10 words

--- week4/day1/word_central/app/controllers/text_inspections_controller.rb
+++ week4/day1/word_central/app/controllers/text_inspections_controller.rb
@@ -14,7 +14,10 @@ class TextInspectionsController < ApplicationController
 
 		@frequent_words = words.inject(Hash.new(0)){|w,e| w[e] += 1
 											w}.sort_by{|k,v| -v}[0..9]
+
 		@longest_sentence = @text.split(".").max
+
+		@longest_words = words.uniq.max_by(10){|w| w.length}.sort_by{|w|-w.length}
 		render "results"
 	end
 end
