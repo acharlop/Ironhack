d89a3ff7a2ce5a8959e63fe99ecae78070d0f64c
Avi Charlop <avicharlop@gmail.com>
2016-04-18 13:54:27 -0400

    text inspection form added

--- week4/day1/word_central/config/routes.rb
+++ week4/day1/word_central/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'site#home'
+  get "/text_inspections/new" => "text_inspections#new"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
