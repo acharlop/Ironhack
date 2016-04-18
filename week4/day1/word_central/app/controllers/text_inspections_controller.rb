fd65f2f8a883ded24aaa7ca42c77289786d5f22a
Avi Charlop <avicharlop@gmail.com>
2016-04-18 15:59:04 -0400

    Added word count table for 10 most frequently used words

--- week4/day1/word_central/app/controllers/text_inspections_controller.rb
+++ week4/day1/word_central/app/controllers/text_inspections_controller.rb
@@ -4,11 +4,16 @@ class TextInspectionsController < ApplicationController
 	end
 	def create
 		@text = params[:text_inspection][:user_text]
-		@word_count = @text.split(" ").length
+		words = @text.split(" ")
+		
+		@word_count = words.length
 		decimal = @word_count / 275.0
 		@seconds = (decimal * 3600).to_i
 		@minutes, @seconds = decimal.divmod(1)
 		@seconds = (@seconds * 60).round
+
+		@frequent_words = words.inject(Hash.new(0)){|w,e| w[e] += 1
+											w}.sort_by{|k,v| -v}[0..9]
 		render "results"
 	end
 end
