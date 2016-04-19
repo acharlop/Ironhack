e5672bce9b577f4983ed8772f9787598d5cfcab9
Avi Charlop <avicharlop@gmail.com>
2016-04-19 14:33:45 -0400

    Form for adding new contact

--- week4/day2/address_book/app/controllers/contacts_controller.rb
+++ week4/day2/address_book/app/controllers/contacts_controller.rb
@@ -2,4 +2,7 @@ class ContactsController < ApplicationController
 	def index
 		@contacts = Contact.all.order(name: :asc)
 	end
+	def new
+		@contact = Contact.new
+	end
 end
--- week4/day2/address_book/config/routes.rb
+++ week4/day2/address_book/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   get "/contacts" => "contacts#index"
+  get "/contacts/new" => "contacts#new"
+  post "/contacts/create" => "contacts#create"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
