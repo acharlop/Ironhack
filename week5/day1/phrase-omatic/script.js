4d8fc21fc8e720a07328a7f46669bf8e5f3ac203
Avi Charlop <avicharlop@gmail.com>
2016-04-25 17:28:01 -0400

    New phrase added from form submit

--- week5/day1/phrase-omatic/script.js
+++ week5/day1/phrase-omatic/script.js
@@ -1,14 +1,19 @@
 // PHRASES METHODS
-function rand_phrase () {
-var phrases = [
+var phrases = new Array(
 	"Say hello to my little friend",
 	"You got to ask yourself one question. Do you feel lucky? Well do ya punk?",
 	"Sticks and stones can break you face, words can get you the death penalty",
 	"Why oh why didn\'t I take the blue pill?"
-]
+)
+
+function rand_phrase () {
 	 $("#phrase").text(
 	 	phrases[Math.floor(Math.random()*phrases.length)]
 	 	)
+}
+
+function new_phrase (text) {
+	phrases.push(text);
 }
 
 // JQUERY FUNCTIONS
@@ -19,6 +24,14 @@ $(document).ready(function() {
 	$("button").click(function(event) {
 		event.preventDefault()
 		rand_phrase()
+	});
+
+	$("form").on('submit', function() {
+		event.preventDefault()
+		var val = $("input").val()
+		// newPhrase(val)
+		new_phrase(val);
+		$("input").val("")
 	});
 
 });
