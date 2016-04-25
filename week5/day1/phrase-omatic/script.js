0044aeabd3ad9e148e9450d9fe6a2eac1c39108b
Avi Charlop <avicharlop@gmail.com>
2016-04-25 16:51:47 -0400

    Random message generated on load

---
+++ week5/day1/phrase-omatic/script.js
@@ -0,0 +1,16 @@
+// PHRASES METHODS
+function rand_phrase () {
+var phrases = [
+	"Say hello to my little friend",
+	"You got to ask yourself one question. Do you feel lucky? Well do ya punk?",
+	"Sticks and stones can break you face, words can get you the death penalty",
+	"Why oh why didn\'t I take the blue pill?"
+]
+	 return phrases[Math.floor(Math.random()*phrases.length)]
+}
+
+// JQUERY FUNCTIONS
+
+$(document).ready(function() {
+	$("#phrase").text(rand_phrase())
+});
