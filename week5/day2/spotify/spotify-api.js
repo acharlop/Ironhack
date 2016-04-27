52a5f9067149386e0bc6411ea55a05cf6d06ea4e
Avi Charlop <avicharlop@gmail.com>
2016-04-27 11:46:16 -0400

    Refactored SpotifyAPI object working all in same ul

--- week5/day2/spotify/spotify-api.js
+++ week5/day2/spotify/spotify-api.js
@@ -1,19 +1,43 @@
-var spotify_api = {
-	url: "https://api.spotify.com/v1/",
-	search_artist,
-	search_albums
+function process_tracks(data) {
+	list = []
+	data.items.forEach( function(track) {
+		list.push({
+			number: track.track_number,
+			name: track.name,
+			url: track.preview_url
+		})
+	})
+
+	var tag = $(".artist-list")
+	list_to_dom(tag,list)
 }
 
-function search_artist(arg) {
-	var url = `${this.url}search?type=artist&query=${arg}`
-	$.get(url, process_data)
+
+
+function list_to_dom(tag,list) {
+	tag.empty()
+	list.forEach( function(item) {
+		var html = api.liTracks(item)
+		tag.append(html)
+	})
 }
 
-function search_albums(id) {
-	var url = `${this.url}artists/${id}/albums/`
-	$.get(url, process_data)
-}
 
+//
+function process_album (data) {
+	list = []
+	data.items.forEach( function(album) {
+		var thumb = "https://placeholdit.imgix.net/~text?txtsize=25&txt=N/A&w=64&h=64"
+		if (album.images.length > 0)
+			thumb = album.images[album.images.length -1].url
+		list.push({
+			name: album.name,
+			id: album.id,
+			img: thumb
+		})
+	})
+	add_to_dom(".artist-list",list,"albums")
+}
 
 
 function process_data(data) {
@@ -28,27 +52,33 @@ function process_data(data) {
 			img: thumb
 		})
 	})
-	add_to_dom(".artist-list",list)
+	add_to_dom(".artist-list",list,"artists")
 }
 
-function add_to_dom(selector, data) {
+function add_to_dom(selector, data, type) {
 	var tag = $(selector)
 	tag.empty()
+	type = (type == "artists" ? "searchAlbums" : "searchTracks")
+
 	data.forEach( function(item) {
 		var html = `
-		<a class="js-${item}" href="#" onclick='search_albums("${item.id}")'>
-			<li>
+		<li>
+			<a class="js-${item}" href="#" onclick='api.${type}("${item.id}")'>
 				<img class="item-thumb" src=${item.img}> 
-				<h2 class="item-name">${item.name.trunc(23)}</h2>
-			</li>
-		</a>
+				<h2 class="item-name">${item.name}</h2>
+			</a>
+		</li>
 		`
 		tag.append(html)
 	})		
 }
 
 
+
+
 String.prototype.trunc = String.prototype.trunc ||
   function(n){
       return (this.length > n) ? this.substr(0,n-1)+'&hellip;' : this;
   };
+
+
--- week5/day2/spotify/spotify.js
+++ week5/day2/spotify/spotify.js
@@ -1,18 +1,69 @@
+var api = new SpotifyAPI()
+
 $(".js-spotify-search-form").on('submit', function(event) {
 	event.preventDefault();
 	var term = $("#input").val()
 						 $("#input").val("")
 
-	spotify_api.search_artist( term, 
-		function(data){ 
-			console.log(data) 
-		})
-});
+	api.searchArtist(term)
+})
 
 
+// 
 
-$(".js-artist").click(function(event) {
-	console.log("here")
-});
+function SpotifyAPI() {
+	var url = "https://api.spotify.com/v1"
+	this.urls = { 
+		artist_url: `${url}/search?type=artist&query=%REPLACE%`,
+		albums_url: `${url}/artists/%REPLACE%/albums/`,
+		tracks_url: `${url}/albums/%REPLACE%/tracks/`
+	}
+}
 
+SpotifyAPI.prototype.search = function(url,arg,fn){
+	var url = url.replace(/%REPLACE%/, arg)
+	$.get(url, fn)
+}
+
+SpotifyAPI.prototype.searchArtist = function(arg) {
+	this.search(this.urls.artist_url, arg, process_data)
+};
+
+SpotifyAPI.prototype.searchAlbums = function(arg) {
+	this.search(this.urls.albums_url, arg, process_album)
+};
+
+SpotifyAPI.prototype.searchTracks = function(arg) {
+	this.search(this.urls.tracks_url, arg, process_tracks)
+};
+
+SpotifyAPI.prototype.liTracks = function(item){
+	return `
+	<li>
+			<h2 class="item-name">${item.number}: ${item.name}</h2>
+			<audio src="${item.url}" controls></audio>
+		</a>
+	</li>`
+}
+
+SpotifyAPI.prototype.liArtists = function(item){
+	this.li_multiple(item,searchAlbums)
+}
+
+SpotifyAPI.prototype.liAlbums = function(item){
+	this.li_multiple(item,searchTracks)
+}
+
+SpotifyAPI.prototype.li_multiple = function(item,next){
+	return `
+		<li>
+			<a class="js-${item}" href="#" onclick='api.${next}("${item.id}")'>
+				<img class="item-thumb" src=${item.img}> 
+				<h2 class="item-name">${item.name}</h2>
+			</a>
+		</li>
+		`
+}
+
+//
 
