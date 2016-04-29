5ee673cc3c41e1256bb7311500c1bbf9d00d275b
Avi Charlop <avicharlop@gmail.com>
2016-04-29 12:47:59 -0400

    Moved bar to update using setInterval

--- week5/day5/spotify-skeleton/script.js
+++ week5/day5/spotify-skeleton/script.js
@@ -1,11 +1,7 @@
 $(".btn-play").on('click', function(event) {
 	var self = $(event.currentTarget)
-	
 	if(!self.hasClass('disabled')){
 		self.toggleClass("playing")
-
-			console.log($("#player").prop("duration"))
-
 		if(self.hasClass("playing")) {
 			$("#player").trigger('play')
 		} else {
@@ -14,15 +10,21 @@ $(".btn-play").on('click', function(event) {
 	}
 });
 
+$("#player").on("playing", function() {
+	intervalID = setInterval(function () {
+    $("progress").attr('value',$("#player").prop("currentTime"))
+	}, 1);
+})
+
+$("#player").on('pause', function() {
+	clearInterval(intervalID)
+});
 
 $("#player").on("durationchange", function() {
 	$("progress").attr('max',$("#player").prop("duration"))
 })
 
-$("#player").on("timeupdate", function() {
-	$("progress").attr('value',$("#player").prop("currentTime"))
-})
-
 $("#player").on("ended", function() {
 	$(".btn-play").toggleClass("playing")
+	clearInterval(intervalID)
 })
