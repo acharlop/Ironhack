560c3990b1668bbb1777f1810df9591597d7b23d
Avi Charlop <avicharlop@gmail.com>
2016-05-03 18:24:11 -0400

    cleaned up javascript file

--- week6/day1/Sandwichr/app/assets/javascripts/sandwich_views.js
+++ week6/day1/Sandwichr/app/assets/javascripts/sandwich_views.js
@@ -1,24 +1,7 @@
-// $.get('/api/sandwiches', function(data) {
-// 	var list = $(".js-sandwich-list")
-// 	data.forEach( function(samich) {
-// 		list.append( buildHtmlSandwich(samich) )
-// 	})
-// })
-// .fail(function(error) {
-// 	console.log(error);
-// })
-
 function buildHtmlSandwich(s) {
 	var list = $(".ingredients-list").append(
 	 ` <li> ${s.name} </li> `)
 }
-
-// 	// $(".js-sandwich-list").on('click', '.sandwich-dot-sexy', function(event) {
-// 	// 	event.preventDefault();
-		
-// 	// 	console.log(this)
-// 	// 	/* Act on the event */
-// 	// });
 
 $(document).on('ready', function() {
 	$(".js-add-ingredient").on('submit', function(event) {
@@ -32,9 +15,6 @@ $(document).on('ready', function() {
 			}
 		}
 
-		// console.log(url)
-		// console.log(ing_id)
-
 		$.post(url, params, function(data) {
 			var calories = data.sandwich.total_calories
 			var ingredient = data.ingredients[data.ingredients.length - 1]
@@ -44,8 +24,6 @@ $(document).on('ready', function() {
 		})
 		.fail(function(error) {
 			console.log(error);
-		})
-		
-	});
-	
-});
+		})		
+	})
+})
