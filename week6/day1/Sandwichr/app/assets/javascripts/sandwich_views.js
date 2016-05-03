f7e10027521afee503c48be66968cd767075725c
Avi Charlop <avicharlop@gmail.com>
2016-05-03 17:02:49 -0400

    index and details page implemented

--- week6/day1/Sandwichr/app/assets/javascripts/sandwich_views.js
+++ week6/day1/Sandwichr/app/assets/javascripts/sandwich_views.js
@@ -1,2 +1,29 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+// $.get('/api/sandwiches', function(data) {
+// 	var list = $(".js-sandwich-list")
+// 	data.forEach( function(samich) {
+// 		list.append( buildHtmlSandwich(samich) )
+// 	})
+// })
+// .fail(function(error) {
+// 	console.log(error);
+// })
+
+// function buildHtmlSandwich(s) {
+// 	return `
+// 		<li>
+// 			<a href="/sandwiches/${s.id}" 
+// 				 class="sandwich-dot-sexy">
+// 				 ${s.name}
+// 			 </a>
+// 		</li>
+// 	`
+// }
+
+// $(document).on('ready', function() {
+// 	// $(".js-sandwich-list").on('click', '.sandwich-dot-sexy', function(event) {
+// 	// 	event.preventDefault();
+		
+// 	// 	console.log(this)
+// 	// 	/* Act on the event */
+// 	// });
+// });
--- week6/day1/Sandwichr/app/controllers/sandwich_views_controller.rb
+++ week6/day1/Sandwichr/app/controllers/sandwich_views_controller.rb
@@ -1,2 +1,15 @@
 class SandwichViewsController < ApplicationController
+	def index
+		@samiches = Sandwich.all
+	end
+	def show
+		@samich = Sandwich.find_by(id: params[:id])
+		unless @samich
+			flash[:alert] = "Sandwich not found"
+			redirect_to sandwiches_path
+			return
+		end
+
+
+	end
 end
